9a70aee997b1606b8fde33142030e2e45a6a4c85
Shad <shad@shad-AO722.(none)>
2012-04-08 00:03:53 -0500

    Added the elisp files

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
-export JEMACS_HOME=/home/shad/mysrc/jemacs/;
-export JEMACS_PATH=$JEMACS_HOME/lib/kawa.jar:$JEMACS_HOME/build:$JEMACS_HOME/lib/ant.jar
+export JEMACS_HOME=.;
+export JEMACS_PATH=$JEMACS_HOME/lib/kawa.jar:$JEMACS_HOME/lib/ant.jar:$JEMACS_HOME/target/jemacs.jar;
 
+rake jemacs.jar;
 #like to have this in rakefile
 $JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lang. -C ./src/gnu/jemacs/lang/NumberOps.scm;
 $JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lang. -C ./src/gnu/jemacs/lang/MiscOps.scm;
 $JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.buffer. -C ./src/gnu/jemacs/buffer/emacs.scm;
+$JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lisp. -C ./src/gnu/jemacs/lisp/primitives.el
+$JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lisp. -C ./src/gnu/jemacs/lisp/alist.el
+$JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lisp. -C ./src/gnu/jemacs/lisp/simple.el  
+$JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lisp. -C ./src/gnu/jemacs/lisp/keymap.el  
+$JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lisp. -C ./src/gnu/jemacs/lisp/keydefs.el  
+$JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lisp. -C ./src/gnu/jemacs/lisp/hanoi.el  
+$JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lisp. -C ./src/gnu/jemacs/lisp/rect.el  
+$JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lisp. -C ./src/gnu/jemacs/lisp/editfns.el  
+$JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lisp. -C ./src/gnu/jemacs/lisp/subr.el  
+$JAVA_HOME/bin/java -classpath $JEMACS_PATH kawa.repl -d target/classes -P gnu.jemacs.lisp. -C ./src/gnu/jemacs/lisp/autoloads.el  
 
 rake jemacs.jar;
+
